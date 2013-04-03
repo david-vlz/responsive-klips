371e7d21e43744e5a642b219d088767f26a0be41
= <Neugebauer_David@web.de>
2013-04-03 15:35:11 +0200

    Korrekturen: Umlaute, Listenpunkte in sidebar.

--- object_information.php
+++ object_information.php
@@ -10,7 +10,7 @@
 		"titel" => "Anorganische Chemie", 
 		"lehrperson" => "Mayer, Piet",
 		"lvNummer" => "00123456",
-		"art" => "&Uumlbung",
+		"art" => "Übung",
 		"semesterstunden" => 2,
 		"teilnehmeranzahl" => 40,
 		"semester" => "SS13",
@@ -18,8 +18,8 @@
 		"sprache" => "DE",
 		"zeit" => "Di, 12:00-13:30 Uhr",
 		"gebaeude" => 1,
-		"beschreibung" => "Sie lernen in diesem Kurs etwas &uumlber anorganische Chemie. Viel Erfolg dabei!",
-		"literatur" => "Mustermann, Max: Einf&uumlhrung in die Anorganische Chemie. Frankfurt/M., 1999.", 
+		"beschreibung" => "Sie lernen in diesem Kurs etwas über anorganische Chemie. Viel Erfolg dabei!",
+		"literatur" => "Mustermann, Max: Einführung in die Anorganische Chemie. Frankfurt/M., 1999.", 
 		"appointments" => Array(1)	// Hier kommen ids von Appointment Objekten rein, die werden später in diese Objekte übersetzt
 	);
 	
@@ -28,7 +28,7 @@
 		"titel" => "Database Systems Advanced", 
 		"lehrperson" => "Bauer, Lucia",
 		"lvNummer" => "00001523",
-		"art" => "&Uumlbung",
+		"art" => "Übung",
 		"semesterstunden" => 2,
 		"teilnehmeranzahl" => 40,
 		"semester" => "SS13",
@@ -44,7 +44,7 @@
 	
 	$courseInfo3 = Array(
 		"id" => 3,
-		"titel" => "&Aumlsthetik der Schrift", 
+		"titel" => "Ästhetik der Schrift", 
 		"lehrperson" => "Blumenfeld, Dr. Jochen",
 		"lvNummer" => "40527",
 		"art" => "Hauptseminar",
@@ -56,9 +56,9 @@
 		"zeit" => "Mo, 16:00-17:30 Uhr",
 		"gebaeude" => 7,
 		"beschreibung" => "Die Schriftlichkeit betont an Texten, wie sie geschrieben worden sind und damit wie Zeichen je situativ anders Schrift werden. 
-		W&aumlhrend solche Fragen klassischerweise von der Editionsphilologie als Fragen der Textkonstitution behandelt worden sind, hat sich um Schriftkulturen
+		Während solche Fragen klassischerweise von der Editionsphilologie als Fragen der Textkonstitution behandelt worden sind, hat sich um Schriftkulturen
 		in den letzten Jahrzehnten ein breites Spektrum kulturwissenschaftlicher Forschungen entwickelt.",
-		"literatur" => "Campe, R&uumldiger: Die Schreibszene. Schreiben. In: Paradoxien, Dissonanzen, Zusammenbr&uumlche. Situationen offener Epistemologie. 
+		"literatur" => "Campe, Rüdiger: Die Schreibszene. Schreiben. In: Paradoxien, Dissonanzen, Zusammenbrüche. Situationen offener Epistemologie. 
 		Hrsg. von Hans Ulrich Gumbrecht und K. Ludwig Pfeiffer. Frankfurt/M., 1991.", 
 		//"appointments" => Array(3)	
 	);
@@ -77,7 +77,7 @@
 		"zeit" => "Fr, 10:00-11:30 Uhr",
 		"gebaeude" => 7,
 		"beschreibung" => "Wir werden folgenden Fragen nachgehen: Wie und warum entsteht syntaktischer Wandel? Wie entfaltet sich syntaktischer Wandel? 
-		Kann man sprach&uumlbergreifende Gesetzm&auml&#946igkeiten in der Entwicklung von grammatischen Prozessen bestimmen?",
+		Kann man sprachübergreifende Gesetzmä&#946;igkeiten in der Entwicklung von grammatischen Prozessen bestimmen?",
 		"literatur" => "Salmons, J.: A History of German. Oxford: Oxford University Press, 2012", 
 		//"appointments" => Array(1, 2)	
 	);
@@ -87,7 +87,7 @@
 		"titel" =>"Bruno Latour und Medien", 
 		"lehrperson" => "Wolferts, Elisa",
 		"lvNummer" => "43502",
-		"art" => "Lekt&uumlrekurs",
+		"art" => "Lektürekurs",
 		"semesterstunden" => 2,
 		"teilnehmeranzahl" => 80,
 		"semester" => "WiSe2012/13",
@@ -95,33 +95,33 @@
 		"sprache" => "DE",
 		"zeit" => "Mo, 10:00-11:30 Uhr",
 		"gebaeude" => 7,
-		"beschreibung" => "Die Forschungen des franz&oumlsischen Philosophen und Soziologen Bruno Latour zeichnen sich durch Interdisziplinarit&aumlt und Lebensn&aumlhe aus. 
-		Er gibt uns Gelegenheit, angespornt durch unsere Lekt&uumlre, die wissenschaftliche Praxis und folglich unser Fach Medienkulturwissenschaft zu reflektieren.",
-		"literatur" => "Latour, Bruno: Der Berliner Schl&uumlssel. Erkundungen eines Liebhabers der Wissenschaften [La clef de Berlin 1993]. Berlin 1996.", 
+		"beschreibung" => "Die Forschungen des französischen Philosophen und Soziologen Bruno Latour zeichnen sich durch Interdisziplinarität und Lebensnähe aus. 
+		Er gibt uns Gelegenheit, angespornt durch unsere Lektüre, die wissenschaftliche Praxis und folglich unser Fach Medienkulturwissenschaft zu reflektieren.",
+		"literatur" => "Latour, Bruno: Der Berliner Schlüssel. Erkundungen eines Liebhabers der Wissenschaften [La clef de Berlin 1993]. Berlin 1996.", 
 		//"appointments" => Array(1, 2)	
 	);
 	
 	$courseInfo6 = Array(
 		"id" => 6,
 		"titel" => "Szenisches Arbeiten", 
-		"lehrperson" => "B&uumlhnenbildner, Dr. Doktor",
+		"lehrperson" => "Bühnenbildner, Dr. Doktor",
 		"lvNummer" => "43540",
 		"art" => "Seminar",
 		"semesterstunden" => 4,
 		"teilnehmeranzahl" => 37,
 		"semester" => "WiSe2012/13",
-		"mail" => "b&uumlhnenbilder.d@uni-koeln.de",
+		"mail" => "bühnenbilder.d@uni-koeln.de",
 		"sprache" => "DE",
 		"zeit" => "Mi, 14:00-15:30 Uhr",
 		"gebaeude" => 6,
 		"beschreibung" => "Wir bauen in diesem Kurs eine szenische Landschaft entweder zu Hamlet oder der Erwartung.",
-		"literatur" => "D&uumlnne, J&oumlrg / G&uumlnzel, Stephan (Hg.): Raumtheorie. Grundlagentexte aus Philosophie und Kulturwissenschaften. Frankfurt/M., 2006", 
+		"literatur" => "Dünne, Jörg / Günzel, Stephan (Hg.): Raumtheorie. Grundlagentexte aus Philosophie und Kulturwissenschaften. Frankfurt/M., 2006", 
 		//"appointments" => Array(1, 2)	
 	);
 	
 	$courseInfo7 = Array(
 		"id" => 7,
-		"titel" => "Popul&aumlre Musik in China, Japan und S&uumldkorea", 
+		"titel" => "Populäre Musik in China, Japan und Südkorea", 
 		"lehrperson" => "Schreiber, Prof. Dr. Herbert",
 		"lvNummer" => "43423",
 		"art" => "Seminar",
@@ -132,11 +132,11 @@
 		"sprache" => "DE",
 		"zeit" => "Di, 12:00-13:30 Uhr",
 		"gebaeude" => 5,
-		"beschreibung" => "Lange Zeit galt popul&aumlre Musik aus Ostasien vielen als Inbegriff unkreativen Kopistentums. 
+		"beschreibung" => "Lange Zeit galt populäre Musik aus Ostasien vielen als Inbegriff unkreativen Kopistentums. 
 		Bis in die 1990er Jahre nahm man in anderen Teilen der Welt dementsprechend auch kaum Notiz von Interpreten aus China, 
-		Japan oder S&uumldkorea. Sp&aumltestens der weltweite Hype, der nicht zuletzt auch in den anglophonen Mutterl&aumlndern der popular music, 
-		den USA und Gro&#946britannien, um den gangnam style des S&uumldkoreaners Psy gemacht wurde, l&aumlsst aber deutlich werden, 
-		dass sich die Flie&#946richtung der weltumspannenden popular music flows grundlegend gewandelt hat.",
+		Japan oder Südkorea. Spätestens der weltweite Hype, der nicht zuletzt auch in den anglophonen Mutterländern der popular music, 
+		den USA und Gro&#946;britannien, um den gangnam style des Südkoreaners Psy gemacht wurde, lässt aber deutlich werden, 
+		dass sich die Flie&#946;richtung der weltumspannenden popular music flows grundlegend gewandelt hat.",
 		"literatur" => "-", 
 		//"appointments" => Array(1, 2)	
 	);
@@ -154,8 +154,8 @@
 		"sprache" => "Arab.",
 		"zeit" => "Di, 8:00-9:30, Do, 8:00-9:30",
 		"gebaeude" => 6,
-		"beschreibung" => "Der zweite Grundkurs in Arabisch baut auf dem Kurs im WiSe2012/13 auf. Ein Bestehen des Vorg&aumlngerkurses ist daher verpflichtend.",
-		"literatur" => " Eckehard Schulz, Modernes Hocharabisch. Lehrbuch & e-Edition. Mit einer Einf&uumlhrung in Hauptdialekte. 
+		"beschreibung" => "Der zweite Grundkurs in Arabisch baut auf dem Kurs im WiSe2012/13 auf. Ein Bestehen des Vorgängerkurses ist daher verpflichtend.",
+		"literatur" => " Eckehard Schulz, Modernes Hocharabisch. Lehrbuch &amp; e-Edition. Mit einer Einführung in Hauptdialekte. 
 		1. Aufl., Leipzig: Edition Hamouda 2011, ISBN 978-3-940075-53-6", 
 		//"appointments" => Array(1, 2)	
 	);
@@ -174,16 +174,16 @@
 		"zeit" => "Di, 8:00-9:30 Uhr, Fr, 8:00-9:30 Uhr",
 		"gebaeude" => 4,
 		"beschreibung" => "Die  Vorlesung 'Lineare Algebra II' ist der zweite Teil einer zweisemestrigen Vorlesung, 
-		die obligatorisch f&uumlr alle Studienanf&aumlnger mit den Studienzielen Bachelor/Master in Mathematik und  Wirtschaftsmathematik ist. 
-		&Uumlbungsscheine werden aufgrund erfolgreicher Mitarbeit in den &Uumlbungen und einer bestandenen Klausur vergeben. Grundz&uumlge der Linearen Algebra: 
-		Vektorr&aumlume, lineare Abbildungen, Skalarprodukte, elementare Eigenwerttheorie, Moduln und euklidische Ringe, multilineare Algebra.",
+		die obligatorisch für alle Studienanfänger mit den Studienzielen Bachelor/Master in Mathematik und  Wirtschaftsmathematik ist. 
+		Übungsscheine werden aufgrund erfolgreicher Mitarbeit in den Übungen und einer bestandenen Klausur vergeben. Grundzüge der Linearen Algebra: 
+		Vektorräume, lineare Abbildungen, Skalarprodukte, elementare Eigenwerttheorie, Moduln und euklidische Ringe, multilineare Algebra.",
 		"literatur" => "G. Fischer: Lineare Algebra.", 
 		//"appointments" => Array(1, 2)	
 	);
 	
 	$courseInfo10 = Array(
 		"id" => 10,
-		"titel" => "Einf&uumlhrung in die theorethische Informatik", 
+		"titel" => "Einführung in die theorethische Informatik", 
 		"lehrperson" => "Humboldt, Andreas",
 		"lvNummer" => "52507",
 		"art" => "Vorlesung",
@@ -194,8 +194,8 @@
 		"sprache" => "DE",
 		"zeit" => "Mo, 12:00-13:30 Uhr, Mi, 12:00-13:30 Uhr",
 		"gebaeude" => 3,
-		"beschreibung" => "Im Kurs wird eine kleine Einf&uumlhrung in die theorethische Informatik gegeben." ,
-		"literatur" => "Sch&oumlning, Uwe: Theoretische Informatik kurz gefasst, 5. Aufl., Spektrum 2008", 
+		"beschreibung" => "Im Kurs wird eine kleine Einführung in die theorethische Informatik gegeben." ,
+		"literatur" => "Schöning, Uwe: Theoretische Informatik kurz gefasst, 5. Aufl., Spektrum 2008", 
 		//"appointments" => Array(1, 2)	
 	);
 	
@@ -204,7 +204,7 @@
 	
 	$guideInfo1 = Array(
 		"id" =>1,
-		"name" =>"Arch&aumlologisches Institut",
+		"name" =>"Archäologisches Institut",
 		"courses" => Array()
 	);
 	$guideInfo2 = Array(
@@ -224,67 +224,67 @@
     );		
 	$guideInfo5 = Array(
 		"id" =>5,
-		"name" =>"Institut f&uumlr Afrikanistik",
+		"name" =>"Institut für Afrikanistik",
 		"courses" => Array()
 	);
 	$guideInfo6 = Array(
 		"id" =>6,
-		"name" =>"Institut f&uumlr Altertumskunde",
+		"name" =>"Institut für Altertumskunde",
 		"courses" => Array()
 	);
 	$guideInfo7 = Array(
 		"id" =>7,
-		"name" =>"Institut f&uumlr deutsche Sprache und Literatur I",
+		"name" =>"Institut für deutsche Sprache und Literatur I",
 		"courses" => Array(3)
 	);
 	$guideInfo8 = Array(
 		"id" =>8,
-		"name" =>"Institut f&uumlr deutsche Sprache und Literatur II",
+		"name" =>"Institut für deutsche Sprache und Literatur II",
 		"courses" => Array(4)
 	);
 	$guideInfo9 = Array(
 		"id" =>9,
-		"name" =>"Institut f&uumlr Ethnologie",
+		"name" =>"Institut für Ethnologie",
 		"courses" => Array(7)
 	);
 	$guideInfo10 = Array(
 		"id" =>10,
-		"name" =>"Institut f&uumlr Evangelische Theologie",
+		"name" =>"Institut für Evangelische Theologie",
 		"courses" => Array()
 	);
 	$guideInfo11 = Array(
 		"id" =>11,
-		"name" =>"Institut f&uumlr Indologie und Tamilistik",
+		"name" =>"Institut für Indologie und Tamilistik",
 		"courses" => Array()
 	);
 	$guideInfo12 = Array(
 		"id" =>12,
-		"name" =>"Institut f&uumlr Katholische Theologie",
+		"name" =>"Institut für Katholische Theologie",
 		"courses" => Array()
 	);
 	$guideInfo13 = Array(
 		"id" =>13,
-		"name" =>"Institut f&uumlr Linguistik",
+		"name" =>"Institut für Linguistik",
 		"courses" => Array()
 	);
 	$guideInfo14 = Array(
 		"id" =>14,
-		"name" =>"Institut f&uumlr Niederlandistik",
+		"name" =>"Institut für Niederlandistik",
 		"courses" => Array()
 	);
 	$guideInfo15 = Array(
 		"id" =>15,
-		"name" =>"Institut f&uumlr Skandivanistik/Fennistik",
+		"name" =>"Institut für Skandivanistik/Fennistik",
 		"courses" => Array()
 	);
 	$guideInfo16 = Array(
 		"id" =>16,
-		"name" =>"Institut f&uumlr Medienkultur- und Theater",
+		"name" =>"Institut für Medienkultur- und Theater",
 		"courses" => Array(5,6)
 	);
 	$guideInfo17 = Array(
 		"id" =>17,
-		"name" =>"Institut f&uumlr Ur- und Fr&uumlhgeschichte",
+		"name" =>"Institut für Ur- und Frühgeschichte",
 		"courses" => Array()
 	);
 	$guideInfo18 = Array(
@@ -294,7 +294,7 @@
 	);
 	$guideInfo19 = Array(
 		"id" =>19,
-		"name" =>"Martin-Buber-Institut f&uumlr Judaistik",
+		"name" =>"Martin-Buber-Institut für Judaistik",
 		"courses" => Array()
 	);
 	$guideInfo20 = Array(
@@ -324,7 +324,7 @@
 	);
 	$guideInfo25 = Array(
 		"id" =>25,
-		"name" =>"Seminar f&uumlr &Aumlgyptologie",
+		"name" =>"Seminar für Ägyptologie",
 		"courses" => Array()
 	);
 	$guideInfo26 = Array(
@@ -339,7 +339,7 @@
 	);
 	$guideInfo28 = Array(
 		"id" =>28,
-		"name" =>"Verbundstudieng&aumlnge",
+		"name" =>"Verbundstudiengänge",
 		"courses" => Array()
 	);
 	$guideInfo29 = Array(
@@ -394,7 +394,7 @@
 	);
 	$guideInfo39 = Array(
 		"id" =>39, 
-		"name" =>"Zentrale Einrichtungen der Fakult&aumlt",
+		"name" =>"Zentrale Einrichtungen der Fakultät",
 		"courses" => Array()
 	);
 	$guideInfo40 = Array(
@@ -414,7 +414,7 @@
 	);
 	$guideInfo43 = Array(
 		"id" =>43, 
-		"name" =>"Veranstaltungen anderer Fakult&aumlten",
+		"name" =>"Veranstaltungen anderer Fakultäten",
 		"courses" => Array()
 	);
 	$guideInfo44 = Array(
@@ -500,19 +500,19 @@
 	
 	$departmentInfo1 = Array (
 		"id" => 1, 
-		"name" => "Mathematische Fakult&aumlt",
+		"name" => "Mathematische Fakultät",
 		"institutes" => Array (29,30,31,32,33,34,27),
 	);
 	
 	$departmentInfo2 = Array (
 		"id" => 2, 
-		"name" => "Philosophische Fakult&aumlt",
+		"name" => "Philosophische Fakultät",
 		"institutes" => Array (2,5,6),
 	);
 	
 	$departmentInfo3 = Array (
 		"id" => 3, 
-		"name" => "Wirtschafts-und Sozialwissenschaftliche Fakult&aumlt",
+		"name" => "Wirtschafts-und Sozialwissenschaftliche Fakultät",
 		"institutes" => Array (35,36,37,38,39,40,41,42,43,44,45,46,27),
 	);
 
--- views/sidebar.php
+++ views/sidebar.php
@@ -1,10 +1,11 @@
 	<ul class="nav nav-tabs nav-stacked">
 		<li class="active"><a href="index.php" >Startseite</a></li>
-		<li>Vorlesungsverzeichnis
-			<ul><a href="index.php?controller=course&action=index">nach Einrichtungen</a></ul>
-			<ul><a href="index.php?controller=search_major&action=index">nach Studiengängen</a></ul>
-			<ul><a href="index.php?controller=data&action=index">(Daten)</a></ul>
-		</li>
+		<li>Vorlesungsverzeichnis</li>
+			<ul>
+				<li class="row2"><a href="index.php?controller=course&action=index">nach Einrichtungen</a></li>
+				<li class="row2"><a href="index.php?controller=search_major&action=index">nach Studieng&auml;ngen</a></li>
+				<li class="row2"><a href="index.php?controller=data&action=index">(Daten)</a></li>
+			</ul>
 		<li><a href="#">Meine Funktionen</a></li>
 		<li><a href="#">Hilfe!</a></li>
 	</ul>
