0118b0bc53a3c9f04fe216e5c6558b069349cda8
= <Neugebauer_David@web.de>
2013-04-03 15:38:08 +0200

    Noch Korrektur Umlaute

--- views/header.php
+++ views/header.php
@@ -1,5 +1,5 @@
 <div id="logo" class="span2">
-	<img src="img/siegel.png" alt="Siegel der Universit&aumlauml;;t zu K&ouml;ln" />
+	<img src="img/siegel.png" alt="Siegel der Universit&auml;t zu K&ouml;ln" />
 </div>
 <div id="banner" class="span10">
 	<img src="img/skyline.png" alt="stilisierte Skyline K&ouml;lns" />
