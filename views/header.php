99c5902283a6f32b448b7ee2430bbd14d00cda29
Stefan Grund <mail@stefangrund.de>
2013-04-22 19:08:59 +0200

    Mobil Anpassungen, Thumbnails für Gebäude

--- views/header.php
+++ views/header.php
@@ -1,1 +1,1 @@
-<img src="img/siegel.png" alt="Siegel der Universit&auml;t zu K&ouml;ln" class="logo" /> <h1><a href="index.php">Universität zu Köln<br>KLIPS <span class="volltext">- Kölner Lehr-, Informations- und Prüfungsservice</span></a></h1>
+<a href="index.php"><img src="img/siegel.png" alt="Siegel der Universit&auml;t zu K&ouml;ln" class="logo" /> <h1><span class="unikoeln">Universität zu Köln</span> KLIPS <span class="volltext">- Kölner Lehr-, Informations- und Prüfungsservice</span></a></h1>
