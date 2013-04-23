7dd41e205e7577e914a31eba54938cd4f43e877d
Stefan Grund <mail@stefangrund.de>
2013-04-23 13:12:03 +0200

    Sidebar-Konflikt behoben

--- views/sidebar.php
+++ views/sidebar.php
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-	<ul class="nav nav-list well">
-		<li><a href="index.php">Startseite</a></li>
-		<li class="nav-header">Vorlesungsverzeichnis</li>
-		<li><a href="index.php?controller=institute&action=index">nach Einrichtungen</a></li>
-		<li><a href="index.php?controller=search_major&action=index">nach Studiengängen</a></li>
-		<li><a href="index.php?controller=data&action=index">(Daten)</a></li>
-		<li class="nav-header">Meine Funktionen</li>
-		<li><a href="#">Funktion 1</a></li>
-		<li><a href="#">Funktion 2</a></li>
-		<li class="nav-header">Hilfe & Service</li>
-		<li><a href="index.php?controller=location&action=index">Gebäude</a></li>
-	</ul>
-=======
-
 <div id="menu" class="well">
 	<i class="icon-align-justify"></i> Navigation <span class="word">anzeigen</span>
 </div>
@@ -20,7 +5,7 @@
 <ul id="navigationlist" class="nav nav-list well">
 	<li><a href="index.php">Startseite</a></li>
 	<li class="nav-header">Vorlesungsverzeichnis</li>
-	<li><a href="index.php?controller=course&action=index">nach Einrichtungen</a></li>
+	<li><a href="index.php?controller=institute&action=index">nach Einrichtungen</a></li>
 	<li><a href="index.php?controller=search_major&action=index">nach Studiengängen</a></li>
 	<li><a href="index.php?controller=data&action=index">(Daten)</a></li>
 	<li class="nav-header">Meine Funktionen</li>
