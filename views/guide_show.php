1dddc0801ad90d36d48b432cff2bcd3927cc8a99
= <Neugebauer_David@web.de>
2013-04-23 13:41:28 +0200

    Links zu Kursen in guide show.

--- views/guide_show.php
+++ views/guide_show.php
@@ -36,7 +36,7 @@
 				<? foreach($degree->courses as $course) { ?>
 				<tr>
 					<td><?= $course->lvNummer?></td>
-					<td><a href='#'><?= $course->titel?></a></td>
+					<td><a href="index.php?controller=course&amp;action=show&amp;id=<?= $course->id ?>"><?= $course->titel?></a></td>
 					<td><?= $course->zeit?></td>
 					<td class="art"><?= $course->art?></td>
 					<td class="modul"><?= $course->modul?></td>
