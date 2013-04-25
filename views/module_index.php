cf19aeb0109259f10148993c7248b5fcf51408b0
= <Neugebauer_David@web.de>
2013-04-25 17:56:10 +0200

    Akkordeon repariert.

--- views/module_index.php
+++ views/module_index.php
@@ -9,34 +9,44 @@
         
               <!-- group collapse -->
 
-<? foreach($this->module as $module) { ?>  
-<div class="accordion" id="accordion2">
-  <div class="accordion-group">
-    <div class="accordion-heading">
-      <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#<?= $module->id?>">
-			<?= $module->titel?>
-      </a>
-    </div>
-    <div id="<?= $module->id?>" class="accordion-body collapse out">
-      <div class="accordion-inner"> 
-      
-      <!-- Schleife, die alle Module aufruft -->
-<? foreach($module->module as $veranstaltung) { ?>
-	
-	&nbsp;&nbsp;&nbsp;<br>
-	 <table id="kursliste" class="table table-striped">
-			<thead>
-	<td>
-	<tr><strong>Titel:</strong> <?= $module->titel; ?></tr>
-	<p><tr><strong>Veranstaltungsart:</strong> <?= $veranstaltung->art; ?></tr></p>
-	<p><tr><strong>Veranstaltung: </strong><?= $veranstaltung->nam; ?></tr></p>
-	<p><tr><strong>Credit Points:</strong> <?= $veranstaltung->cp; ?></tr></p>
-	<p><tr><strong>Note: </strong><?= $veranstaltung->mark; ?></tr></p> 
-	</thead>
-	</table>
-				
+	<? foreach($this->module as $module) { ?>  
+		<div class="accordion" id="accordion2">
+		  <div class="accordion-group">
+		    <div class="accordion-heading">
+		      <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#<?= $module->id?>">
+					<?= $module->titel?>
+		      </a>
+		    </div>
+		    <div id="<?= $module->id?>" class="accordion-body collapse out">
+				<div class="accordion-inner"> 
+		      
+			      <!-- Schleife, die alle Module aufruft -->
+				<? foreach($module->module as $veranstaltung) { ?>
+					
+					&nbsp;&nbsp;&nbsp;<br>
+					 <table id="kursliste" class="table table-striped">
+							<thead>
+					<td>
+					<tr><strong>Titel:</strong> <?= $module->titel; ?></tr>
+					<p><tr><strong>Veranstaltungsart:</strong> <?= $veranstaltung->art; ?></tr></p>
+					<p><tr><strong>Veranstaltung: </strong><?= $veranstaltung->nam; ?></tr></p>
+					<p><tr><strong>Credit Points:</strong> <?= $veranstaltung->cp; ?></tr></p>
+					<p><tr><strong>Note: </strong><?= $veranstaltung->mark; ?></tr></p> 
+					</thead>
+					</table>
+								
 				<? } ?>
 
+				</div>
+			</div>
+			</div>
+		</div>
+	<? } ?>
 </div>
-</div>
-<? } ?>
+
+<p>
+  <button class="btn danger" data-toggle="collapse" data-target="#collapseDemoWrap2">
+    <h5>Prüfungsleistungen in Medieninformatik <i class="icon-ok-circle"></i>42/90 CPs</h5>
+  </button>
+</p>
+ 
